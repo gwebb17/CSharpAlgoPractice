83b8b314c930304e01b42c069359b3c50dab403f
gwebb17 <gwebb1723@gmail.com>
2022-12-04 16:51:26 -0800

    Add project files.

---
+++ DateTimePractice/Clock.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTimePractice
+{
+    public class Clock
+    {
+        public DateTime baseTime { get; set; }
+        public DateTime militaryTime { get; set; }
+
+
+
+        public int Method1(out int methodOneResult, int answer1A, int answer2A)
+        {
+            methodOneResult = answer1A + answer2A;
+            Console.WriteLine(methodOneResult);
+            return methodOneResult;
+        }
+
+        public int NextMethod(out int NextMethodResult, int answer3A)
+        {
+            NextMethodResult = answer3A + 1;
+            Console.WriteLine(NextMethodResult);
+            return NextMethodResult;
+        }
+
+        public int AgeMethod(out int AgeMethodResult, int answer4A)
+        {
+            AgeMethodResult = answer4A * 365;
+            Console.WriteLine(AgeMethodResult);
+            return AgeMethodResult;
+        }
+
+        public int RectangleMethod(out int RectangleMethodResult, int answer5A, int answer5B)
+        {
+            RectangleMethodResult = 2 * answer5A * answer5B;
+            Console.WriteLine(RectangleMethodResult);
+            return RectangleMethodResult;
+        }
+
+        public string MonthMethod(out string MonthMethodResult, int answer6A)
+        {
+            
+            if (answer6A == 1)
+            {
+                MonthMethodResult = "January";
+                Console.Write(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 2)
+            {
+                MonthMethodResult = "February";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 3)
+            {
+                MonthMethodResult = "March";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 4)
+            {
+                MonthMethodResult = "April";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 5)
+            {
+                MonthMethodResult = "May";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 6)
+            {
+                MonthMethodResult = "June";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 7)
+            {
+                MonthMethodResult = "July";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 8)
+            {
+                MonthMethodResult = "August";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 9)
+            {
+                MonthMethodResult = "September";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 10)
+            {
+                MonthMethodResult = "October";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 11)
+            {
+                MonthMethodResult = "November";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else if (answer6A == 12)
+            {
+                MonthMethodResult = "December";
+                Console.WriteLine(MonthMethodResult);
+                return MonthMethodResult;
+            }
+            else
+            {
+                MonthMethodResult = "Nothing";
+                return MonthMethodResult;
+            }
+            
+        }
+
+        //Loop through an array of user entered values and multiply each item in the array by the array's length
+        //Return each result 
+        public void ArrayMethod(int answer7A, int answer7B, int answer7C, int answer7D)
+        {
+            int[] aRay = { answer7A, answer7B, answer7C, answer7D };
+
+            for (int i = 0; i < aRay.Length; i++)
+            {
+                int j = aRay[i] * aRay.Length;
+                Console.WriteLine(j);
+            }
+
+
+        }
+
+
+        //Write a C# Sharp program that takes four numbers as input to calculate and print the average
+        public double AverageMethod(out double AverageMethodResult, List<int>list8A)
+        {
+                int answer8XA = list8A[0] + list8A[1] + list8A[2] + list8A[3];
+                AverageMethodResult = answer8XA / list8A.Count;  //.Count is similar in use to .Length but for Lists
+                Console.WriteLine(AverageMethodResult);
+                return AverageMethodResult;           
+        }
+
+
+
+    }
+}
---
+++ DateTimePractice/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DateTimePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Clock clockenspiel = new Clock();
+
+            //#1
+            Console.WriteLine("Enter number 1");
+            int answer1A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            int answer2A = Convert.ToInt32(Console.ReadLine());
+            clockenspiel.Method1(out int methodOneResult, answer1A, answer2A);
+
+            //#2
+            Console.WriteLine("Enter an amount of minutes");
+            int answer1B = Convert.ToInt32(Console.ReadLine());
+            int result1B = answer1B *= 60;
+            Console.WriteLine("The amount in seconds is: " + result1B + ".");
+
+            //#3
+            Console.WriteLine("Enter a number to increment");
+            int answer3A = Convert.ToInt32(Console.ReadLine());
+            clockenspiel.NextMethod(out int NextMethodResult, answer3A);
+
+            //#4
+            Console.WriteLine("Enter your age: ");
+            int answer4A = Convert.ToInt32(Console.ReadLine());
+            clockenspiel.AgeMethod(out int AgeMethodResult, answer4A);
+
+
+            //#5
+            Console.WriteLine("Enter length of rectangle ");
+            int answer5A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width of rectangle ");
+            int answer5B = Convert.ToInt32(Console.ReadLine());
+            clockenspiel.RectangleMethod(out int RectangleMethodResult, answer5A, answer5B);
+
+            //#6
+            Console.WriteLine("Enter # between 1-12");
+            int answer6A = Convert.ToInt32(Console.ReadLine());
+            clockenspiel.MonthMethod(out string MonthMethodResult, answer6A);
+
+            //#7
+            //Loop through an array of user entered values and multiply each item in the array by the array's length
+            //Return each result 
+            Console.WriteLine("\nEnter array #1");
+            int answer7A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter array #2");
+            int answer7B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter array #3");
+            int answer7C = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter array #4");
+            int answer7D = Convert.ToInt32(Console.ReadLine());
+            clockenspiel.ArrayMethod(answer7A, answer7B, answer7C, answer7D);
+
+            //#8
+            //Write a C# Sharp program that takes user inputs, turns them into a List, and finds the average of the List 
+            Console.WriteLine("#1");
+            int answer8A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("#2");
+            int answer8B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("#3");
+            int answer8C = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("#4");
+            int answer8D = Convert.ToInt32(Console.ReadLine());
+            List<int> list8A = new List<int>(){answer8A, answer8B, answer8C, answer8D};   //Have to instantiate first since its a list
+            clockenspiel.AverageMethod(out double AverageMethodResult, list8A);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
